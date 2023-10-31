d08caed2a9065922918bb1aea12aed347c897aca
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-31 19:23:12 +0100

    refactor: move img folder into src folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname + '/dist'),
+    path: path.resolve(__dirname + 'dist'),
     clean: true,
   },
   module: {
